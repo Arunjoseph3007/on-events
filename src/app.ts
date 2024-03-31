b5b1cd11ef3ece0b2f295e50afb70e65e6f4942f
Arun Joseph <arunjoseph3007@gmail.com>
2024-03-31 13:07:15 +0530

    Google oauth new scopes

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/static", express.static("./public"));
+app.use(express.static("./public"));
 
 // Health check route
 app.get("/", (_, res) => res.json({ hello: "world" }));
