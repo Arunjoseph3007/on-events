3520728e93c0e3e26147eebcf8b3a8b2b38d0df1
Arun Joseph <arunjoseph3007@gmail.com>
2024-03-31 17:47:03 +0530

    Google oauth get refresh token

--- src/actions/gsheets/addRow.ts
+++ src/actions/gsheets/addRow.ts
@@ -6,10 +6,18 @@ export default async function addRow(sheetId: string, values: string[]) {
       `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/Sheet1!A1:C6:append`,
       {
         range: "Sheet1!A1:C6",
-        majorDimension: "ROW",
+        majorDimension: "ROWS",
         values: [values],
       },
-      { params: { key: process.env.GSHEETS_API_KEY } }
+      {
+        headers: {
+          Authorization:
+            "Bearer ya29.a0Ad52N3-Hx0nz7QR-Z0UrEpJAHmPt2Hy2-E2PpVHxL-Zgql19wyjLNaZmLaWp7_Hw7z7plu2MLYTMwl1RdzfcDTewncXJQnIw-M5_DhzsGYpgRqyD8sWeOalMdpfsWDHFcHVHOc4PuunU3vH24t4u8MOY093GNeYT9PkaCgYKAW8SARESFQHGX2MivPm2HYbP-o_i5DWOzTObMQ0170",
+        },
+        params: {
+          valueInputOption: "RAW",
+        },
+      }
     );
 
     console.log(data);
