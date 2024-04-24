7c3026680aaf9a9645edfcfc4de2ebe6dbba2587
Arun Joseph <arunjoseph3007@gmail.com>
2024-04-24 12:26:50 +0530

    next level test

--- api/index.ts
+++ api/index.ts
@@ -9,33 +9,44 @@ import path from "path";
 //   "utf-8"
 // );
 
-const template=''
+const template = "<html><!--ssr-outlet--></html>";
 
-app.use(
-  express.static(
-    path.resolve(__dirname, "..", ".vercel/output/static/client"),
-    {
-      index: false,
-    }
-  )
-);
+// app.use(
+//   express.static(
+//     path.resolve(__dirname, "..", ".vercel/output/static/client"),
+//     {
+//       index: false,
+//     }
+//   )
+// );
 
 app.use("*", async (req, res) => {
   try {
-    console.error(fs.readdirSync(path.resolve(__dirname, "..")));
-    console.error(fs.readdirSync(path.resolve(__dirname, "..", ".vercel")));
-    console.error(fs.readdirSync(path.resolve(__dirname, "..", ".vercel/output")));
-    console.error(
-      fs.readdirSync(path.resolve(__dirname, "..", ".vercel/output/client"))
-    );
-    const { render } = await import(
-      // @ts-ignore
-      "../.vercel/output/static/server/server.mjs"
-    );
-    const rendered = (await render({ path: req.originalUrl })) as string;
+    const d = (...a: string[]) => {
+      try {
+        console.error("Directory", path.resolve(...a));
+        console.error("Content", fs.readdirSync(path.resolve(...a)));
+      } catch (error) {
+        console.error("Input", ...a, "failed");
+      }
+    };
+    d(__dirname);
+    d(__dirname, "..");
+    d(__dirname, "..", "src");
+    d(__dirname, "..", ".vercel");
+    d(__dirname, "..", ".vercel/output");
+    d(__dirname, "..", ".vercel/output/client");
 
-    const html = template.replace(`<!--ssr-outlet-->`, rendered);
-    res.status(200).set({ "Content-Type": "text/html" }).end(html);
+    // const { render } = await import(
+    //   // @ts-ignore
+    //   "../.vercel/output/static/server/server.mjs"
+    // );
+    // const rendered = (await render({ path: req.originalUrl })) as string;
+
+    // const html = template.replace(`<!--ssr-outlet-->`, rendered);
+    // res.status(200).set({ "Content-Type": "text/html" }).end(html);
+
+    res.send("hello");
   } catch (error) {
     res.status(500).end(error);
   }
