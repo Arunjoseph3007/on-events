115bbdd004ffbcdd266c13af05e5f7036e7796ea
Arun Joseph <arunjoseph3007@gmail.com>
2024-04-30 10:54:15 +0530

    Vercel attempt again

--- api/index.ts
+++ api/index.ts
@@ -4,53 +4,26 @@ import fs from "fs";
 import errorHandlingMiddleware from "../src/middlewares/errorHandling";
 import path from "path";
 
-// const template = fs.readFileSync(
-//   path.resolve(__dirname, "..", ".vercel/output/static/client/index.html"),
-//   "utf-8"
-// );
+const PORT = 3000;
 
-const template = "<html><!--ssr-outlet--></html>";
+app.use(
+  express.static(path.resolve(__dirname, "client"), {
+    index: false,
+  })
+);
 
-// app.use(
-//   express.static(
-//     path.resolve(__dirname, "..", ".vercel/output/static/client"),
-//     {
-//       index: false,
-//     }
-//   )
-// );
-
-app.use("*", async (req, res) => {
+app.use("*", async (_, res) => {
   try {
-    const d = (...a: string[]) => {
-      try {
-        console.error("Directory", path.resolve(...a));
-        console.error("Content", fs.readdirSync(path.resolve(...a)));
-      } catch (error) {
-        console.error("Input", ...a, "failed");
-      }
-    };
-    d(__dirname);
-    d(__dirname, "..");
-    d(__dirname, "..", "src");
-    d(__dirname, "..", ".vercel");
-    d(__dirname, "..", ".vercel/output");
-    d(__dirname, "..", ".vercel/output/client");
-    d(__dirname, "..", ".vercel/output/server");
-    d(__dirname, "..", ".vercel/output/static");
-    d(__dirname, "..", ".vercel/output/static/client");
-    d(__dirname, "..", ".vercel/output/static/server");
+    const template = fs.readFileSync(
+      path.resolve(__dirname, "client/index.html"),
+      "utf-8"
+    );
 
-    // const { render } = await import(
-    //   // @ts-ignore
-    //   "../.vercel/output/static/server/server.mjs"
-    // );
-    // const rendered = (await render({ path: req.originalUrl })) as string;
-
-    // const html = template.replace(`<!--ssr-outlet-->`, rendered);
-    // res.status(200).set({ "Content-Type": "text/html" }).end(html);
-
-    res.send("hello");
+    // @ts-ignore
+    const { render } = await import("../dist/server/server.mjs");
+    // @ts-ignore
+    const html = template.replace(`<!--ssr-outlet-->`, render);
+    res.status(200).set({ "Content-Type": "text/html" }).end(html);
   } catch (error) {
     res.status(500).end(error);
   }
@@ -58,4 +31,6 @@ app.use("*", async (req, res) => {
 
 app.use(errorHandlingMiddleware);
 
-export default app;
+app.listen(PORT, () => {
+  console.log("App running on port:", PORT);
+});
