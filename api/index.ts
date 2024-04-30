b604f4b59c37b7f3fc7585dd5d7492aeb173340f
Arun Joseph <arunjoseph3007@gmail.com>
2024-04-30 11:14:45 +0530

    Were Back bois

--- api/index.ts
+++ api/index.ts
@@ -6,26 +6,24 @@ import path from "path";
 
 const PORT = 3000;
 
-// app.use(
-//   express.static(path.resolve(__dirname, "client"), {
-//     index: false,
-//   })
-// );
+app.use(
+  express.static(path.resolve(__dirname, "..", "dist/client"), {
+    index: false,
+  })
+);
 
 app.use("*", async (_, res, next) => {
   try {
-    console.log(fs.readdirSync(path.resolve(__dirname, "..", "dist/client")));
-    // const template = fs.readFileSync(
-    //   path.resolve(__dirname, "client/index.html"),
-    //   "utf-8"
-    // );
+    const template = fs.readFileSync(
+      path.resolve(__dirname, "..", "dist/client/index.html"),
+      "utf-8"
+    );
 
-    // // @ts-ignore
-    // const { render } = await import("../dist/server/server.mjs");
-    // // @ts-ignore
-    // const html = template.replace(`<!--ssr-outlet-->`, render);
-    // res.status(200).set({ "Content-Type": "text/html" }).end(html);
-    res.send("hello");
+    // @ts-ignore
+    const { render } = await import("../dist/server/server.mjs");
+    // @ts-ignore
+    const html = template.replace(`<!--ssr-outlet-->`, render);
+    res.status(200).set({ "Content-Type": "text/html" }).end(html);
   } catch (error) {
     next(error);
   }
