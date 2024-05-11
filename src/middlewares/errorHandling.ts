216930f09f237f4c19e70568ccbd12d1fd70b44d
Arun Joseph <arunjoseph3007@gmail.com>
2024-05-11 18:32:46 +0530

    better error handling

--- src/middlewares/errorHandling.ts
+++ src/middlewares/errorHandling.ts
@@ -2,6 +2,7 @@ import type { NextFunction, Request, Response } from "express";
 import { ZodError } from "zod";
 import UnAuthorizedError from "../error/unauthorized";
 import NotFoundError from "../error/notFound";
+import CustomHttpError from "../error/httpError";
 
 export default function errorHandlingMiddleware(
   err: Error,
@@ -11,27 +12,31 @@ export default function errorHandlingMiddleware(
 ) {
   if (err instanceof ZodError) {
     const json = JSON.parse(err.message);
-    res.status(500).json({
+    return res.status(500).json({
       error: json || "Something went wrong",
       message: "Data validation error",
     });
-    return;
   }
 
   if (err instanceof UnAuthorizedError) {
-    res.status(401).json({
+    return res.status(401).json({
       error: err.error,
       message: err.message,
     });
-    return;
   }
 
   if (err instanceof NotFoundError) {
-    res.status(404).json({
+    return res.status(404).json({
       error: err,
       message: err.message,
     });
-    return;
+  }
+
+  if (err instanceof CustomHttpError) {
+    return res.status(err.code).json({
+      error: err,
+      message: err.message,
+    });
   }
 
   res.status(500).json({ message: err.message || "Something went wrong" });
--- src/utils/resErrors.ts
+++ src/utils/resErrors.ts
@@ -2,16 +2,33 @@ import type { Express } from "express";
 import NotFoundError from "../error/notFound";
 import UnAuthorizedError from "../error/unauthorized";
 import { AssertionError } from "assert";
+import CustomHttpError from "../error/httpError";
 
 declare module "express-serve-static-core" {
   interface Response {
     notFound: (message?: string) => never;
     unauthorized: (message?: string) => never;
+    error: (code: number, message?: string) => never;
     assert: <T>(condition: T) => asserts condition is NonNullable<T>;
+  }
+
+  interface Express {
+    handleError: <TError extends Error>(
+      cls: new () => TError,
+      func: (p: TError) => any
+    ) => void;
+    _errorStack: Array<[Error, (p: Error) => any]>;
   }
 }
 
 export default function setupResErrors(app: Express) {
+  app._errorStack = [];
+
+  app.handleError = (cls, func) => {
+    // @ts-ignore
+    app._errorStack.push([cls, func]);
+  };
+
   app.response.notFound = (message) => {
     throw new NotFoundError(message);
   };
@@ -28,5 +45,12 @@ export default function setupResErrors(app: Express) {
         expected: "truthy",
       });
     }
+  };
+
+  app.response.error = (code, message) => {
+    throw new CustomHttpError(
+      code,
+      message || "Something went wrong. HTTP Error"
+    );
   };
 }
